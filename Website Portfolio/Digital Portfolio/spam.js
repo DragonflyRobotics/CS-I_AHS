e29922cd83cf16105d0af0513c7c21becf3a85a5
Krishna Shah <kshahusa@gmail.com>
2023-09-14 22:37:06 -0500

    Added digital portfolio

---
+++ Website Portfolio/Digital Portfolio/spam.js
@@ -0,0 +1,7 @@
+function spam_em(){
+  for (var i = 0; i < 10; i++) {
+      setTimeout(() => {
+  	  window.open("https://www.w3schools.com/js/js_jquery_selectors.asp"  );
+      }, 200);
+  }
+}
---
+++ Website Portfolio/style-copy.sh
@@ -0,0 +1,19 @@
+# /bin/bash
+
+cp www/style.css www/Journals/
+cp www/style.css~ www/Journals/
+cp www/style.css "/home/krishna-shah/www/Digital Portfolio/CS-I"
+cp www/style.css~ "/home/krishna-shah/www/Digital Portfolio/CS-I"
+cp www/style.css "/home/krishna-shah/www/Digital Portfolio/"
+cp www/style.css~ "/home/krishna-shah/www/Digital Portfolio/"
+cp www/style.css~ "/home/krishna-shah/www/Digital Portfolio/CS-I/Journals"
+cp www/Journals/* www/Digital\ Portfolio/CS-I/Journals/
+cp www/index.html www/Digital\ Portfolio/CS-I/journal_listing.html
+cp www/style.css "/home/krishna-shah/www/Digital Portfolio/CS-I/Projects/ISP"
+cp ~/style-copy.sh www
+
+
+#chmod 777 -R Journals
+chmod 777 -R www
+#chmod 777 -R "Digital Portfolio"
+
